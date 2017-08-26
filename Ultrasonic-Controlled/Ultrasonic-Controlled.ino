6768c7da90b03a6c4519549fcdad615a456c34f4
puqeko <puqeko@gmail.com>
2017-08-26 13:25:56 +1200

    cleaned up with comments, added gitignore

--- Ultrasonic-Controlled/Ultrasonic-Controlled.ino
+++ Ultrasonic-Controlled/Ultrasonic-Controlled.ino
@@ -1,50 +1,43 @@
 /*
- *
- * DDS Sine Generator mit ATMEGS 168
- * Timer2 generates the  31250 KHz Clock Interrupt
- *
- * KHM 2009 /  Martin Nawrath
- * Kunsthochschule fuer Medien Koeln
- * Academy of Media Arts Cologne
-
+ * ENEL200 Design Project (Theremin)
+ * Ultrasonic controlled PWM sinewave generator.
+ * 
+ * Thomas Morrison, 26/08/17.
  */
 
 #include "avr/pgmspace.h"
 #include <math.h>
 
-// table of 256 sine values / one sine period / stored in flash memory
+// 256 sine values, one sine period, stored in flash memory
 const PROGMEM  unsigned char sine256[]  = {
-  127,130,133,136,139,143,146,149,152,155,158,161,164,167,170,173,176,178,181,184,187,190,192,195,198,200,203,205,208,210,212,215,217,219,221,223,225,227,229,231,233,234,236,238,239,240,
-  242,243,244,245,247,248,249,249,250,251,252,252,253,253,253,254,254,254,254,254,254,254,253,253,253,252,252,251,250,249,249,248,247,245,244,243,242,240,239,238,236,234,233,231,229,227,225,223,
-  221,219,217,215,212,210,208,205,203,200,198,195,192,190,187,184,181,178,176,173,170,167,164,161,158,155,152,149,146,143,139,136,133,130,127,124,121,118,115,111,108,105,102,99,96,93,90,87,84,81,78,
-  76,73,70,67,64,62,59,56,54,51,49,46,44,42,39,37,35,33,31,29,27,25,23,21,20,18,16,15,14,12,11,10,9,7,6,5,5,4,3,2,2,1,1,1,0,0,0,0,0,0,0,1,1,1,2,2,3,4,5,5,6,7,9,10,11,12,14,15,16,18,20,21,23,25,27,29,31,
-  33,35,37,39,42,44,46,49,51,54,56,59,62,64,67,70,73,76,78,81,84,87,90,93,96,99,102,105,108,111,115,118,121,124
-
+  127,130,133,136,139,143,146,149,152,155,158,161,164,167,170,173,176,178,181,184,187,190,192,
+  195,198,200,203,205,208,210,212,215,217,219,221,223,225,227,229,231,233,234,236,238,239,240,
+  242,243,244,245,247,248,249,249,250,251,252,252,253,253,253,254,254,254,254,254,254,254,253,
+  253,253,252,252,251,250,249,249,248,247,245,244,243,242,240,239,238,236,234,233,231,229,227,
+  225,223,221,219,217,215,212,210,208,205,203,200,198,195,192,190,187,184,181,178,176,173,170,
+  167,164,161,158,155,152,149,146,143,139,136,133,130,127,124,121,118,115,111,108,105,102,99,
+  96,93,90,87,84,81,78,76,73,70,67,64,62,59,56,54,51,49,46,44,42,39,37,35,33,31,29,27,25,23,
+  21,20,18,16,15,14,12,11,10,9,7,6,5,5,4,3,2,2,1,1,1,0,0,0,0,0,0,0,1,1,1,2,2,3,4,5,5,6,7,9,
+  10,11,12,14,15,16,18,20,21,23,25,27,29,31,33,35,37,39,42,44,46,49,51,54,56,59,62,64,67,
+  70,73,76,78,81,84,87,90,93,96,99,102,105,108,111,115,118,121,124
 };
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 
-double dfreq;
+double dfreq = 0.0;
 // const double refclk=31372.549;  // =16MHz / 510
 const double refclk=31376.6;      // measured
 
 // variables used inside interrupt service declared as voilatile
-volatile byte icnt;              // var inside interrupt
-volatile byte icnt1;             // var inside interrupt
-volatile byte c4ms;              // counter incremented all 4ms
-volatile unsigned long phaccu;   // pahse accumulator
+volatile unsigned long phaccu;   // phase accumulator
 volatile unsigned long tword_m;  // dds tuning word m
 
 #define echoPin 7   // Echo Pin
 #define trigPin 8   // Trigger Pin
 #define PWMPin 11   // PWM Output
 
-double duration, distance;  // Duration used to calculate distance
-int error_signal, command_signal = 100;
-
-double prev[7] = {0.0};
-double filter[] = //{1.0/64.0, 6.0/64.0, 15.0/64.0, 20.0/64.0, 15.0/64.0, 6.0/64.0, 1.0/64.0};
-{
+double distanceBuffer[7] = {0.0};
+double filterKernal[] = {1.0/64.0, 6.0/64.0, 15.0/64.0, 20.0/64.0, 15.0/64.0, 6.0/64.0, 1.0/64.0};
 int nextPos = 3;
 
 void setup()
@@ -62,72 +55,76 @@ void setup()
   cbi (TIMSK0,TOIE0);              // disable Timer0 !!! delay() is now not available
   sbi (TIMSK2,TOIE2);              // enable Timer2 Interrupt
 
-  dfreq=1000.0;                    // initial output frequency = 1000.o Hz
   tword_m=pow(2,32)*dfreq/refclk;  // calulate DDS new tuning word 
-
 }
 
-// modulus
-int mymod(int a, int b) {
-  int res = a % b;
-  if (res < 0) res += b;
-  return res;
+// true modulus, result is allways a positive int
+int modp(int a, int b) {
+  return (a + b) % b;
 }
 
-double dist_at(int newDistance) {
-  double sum = 0;
+// apply filter kernal to distance data (i.e. smooth the data)
+double applyFilter(float nextDistance) {
+  // update newest value
+  distanceBuffer[nextPos] = nextDistance;
+
+  double sum = 0.0;
   for (int i = 0; i < 7; i++) {
-    int disti = mymod(newDistance + i + 1, 7);
-    sum += prev[disti] * filter[i];
+    int disti = modp(nextPos + i + 1, 7);
+    sum += distanceBuffer[disti] * filterKernal[i];
   }
+
+  // increment for next time
+  nextPos = modp(nextPos + 1, 7);
   return sum;
 }
+
+// get distance in cm to object
+double getUltrasonicDistance() {
+  // send pulse to ultrasonic sensor
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW);
+
+  // the time until a pulse is received from the echo pin
+  unsigned long duration = pulseIn(echoPin, HIGH);
+  
+  return duration / 58.2; // conversion of time to distance (in cm)
+}
+
+double distance = 0.0;  // Duration used to calculate distance
+#define MAX_ULTRASONIC_DISTANCE 100  // in cm
+#define MAX_OUTPUT_FREQENCY 1500.0
 
 void loop()
 {
   while(1) {
-     //if (c4ms > 10) {//250) {                 // timer / wait fou a full second
-      c4ms=0;
+    
+    distance = getUltrasonicDistance();
+    double smoothDistance = applyFilter(distance);
+    
+    // dfreq 0 -> 1000 Hz
+    if (smoothDistance > MAX_ULTRASONIC_DISTANCE) {
+      dfreq = 0;
+    } else {
+      dfreq  smoothDistance * 50.0 - 50;  // shift and scale
+      dfreq = min(dfreq, MAX_OUTPUT_FREQENCY);
+    }
 
-      digitalWrite(trigPin, LOW);  // pulse to ultrasonic sensor
-      delayMicroseconds(2); 
-      digitalWrite(trigPin, HIGH);
-      delayMicroseconds(10); 
-      digitalWrite(trigPin, LOW);
-      duration = (double) pulseIn(echoPin, HIGH);  // read responce
-      
-      distance = duration/58.2; // conversion of time to distance (cm)
+    cbi (TIMSK2,TOIE2);              // disble Timer2 Interrupt
+    tword_m=pow(2,32)*dfreq/refclk;  // calulate DDS new tuning word
+    sbi (TIMSK2,TOIE2);              // enable Timer2 Interrupt 
 
-      prev[nextPos] = distance;
-      double smoothDistance = dist_at(nextPos);
-      nextPos = mymod(nextPos + 1, 7);
-      
-       if (smoothDistance > 100) dfreq = 0;
-      else {
-        dfreq=smoothDistance * 50.0 - 50;             // read Poti on analog pin 0 to adjust output frequency from 0..1023 Hz
-      
-        dfreq = min(dfreq, 1500.0);
-//        dfreq *= 24.0/1500.0;
-//        dfreq = round(dfreq);
-//        dfreq *= 1500.0/24.0;
-      }
-
-      cbi (TIMSK2,TOIE2);              // disble Timer2 Interrupt
-      tword_m=pow(2,32)*dfreq/refclk;  // calulate DDS new tuning word
-      sbi (TIMSK2,TOIE2);              // enable Timer2 Interrupt 
-
-      Serial.print(dfreq);
-      Serial.print("  ");
-      Serial.print(smoothDistance);
-      Serial.print("  ");
-      Serial.println(distance);
-   // }
-
-   //sbi(PORTD,6); // Test / set PORTD,7 high to observe timing with a scope
-   //cbi(PORTD,6); // Test /reset PORTD,7 high to observe timing with a scope
+    Serial.print(dfreq);
+    Serial.print("  ");
+    Serial.print(smoothDistance);
+    Serial.print("  ");
+    Serial.println(distance);
   }
  }
-//******************************************************************
+
 // timer2 setup
 // set prscaler to 1, PWM mode to phase correct PWM,  16000000/510 = 31372.55 Hz clock
 void Setup_timer2() {
@@ -152,19 +149,8 @@ void Setup_timer2() {
 // FOUT = (M (REFCLK)) / (2 exp 32)
 // runtime : 8 microseconds ( inclusive push and pop)
 ISR(TIMER2_OVF_vect) {
-
-  //sbi(PORTD,7);          // Test / set PORTD,7 high to observe timing with a oscope
-
   phaccu=phaccu+tword_m; // soft DDS, phase accu with 32 bits
   icnt=phaccu >> 24;     // use upper 8 bits for phase accu as frequency information
                          // read value fron ROM sine table and send to PWM DAC
-
-  OCR2A=pgm_read_byte_near(sine256 + icnt);    
-
-  if(icnt1++ == 125) {  // increment variable c4ms all 4 milliseconds
-    c4ms++;
-    icnt1=0;
-   }   
-
- //cbi(PORTD,7);            // reset PORTD,7
+  OCR2A=pgm_read_byte_near(sine256 + icnt);
 }
