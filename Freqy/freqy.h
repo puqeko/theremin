6f11eae85ba62b1e2f733d309050de53111dfa0c
puqeko <puqeko@gmail.com>
2017-09-20 17:13:18 +1200

    remove test

--- Freqy/freqy.h
+++ Freqy/freqy.h
@@ -1,8 +1,6 @@
 // Freqy.h - Freqency measurment on Arduino Uno.
 // ENEL200 Design Project, building a theremin.
 // 17-09-17, Group 13
-//
-// Use one timer as a reference, then check the count of the clocked timer every sampleTimeMSecs
 
 #include "Timers.h"
 
