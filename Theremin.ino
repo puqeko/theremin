32d68b4bd00891d3d4d4ca55b3343693de957022
Ryan Hall <31651110+RyanNKHall@users.noreply.github.com>
2017-10-11 03:59:48 +1300

    Add files via upload

--- Theremin.ino
+++ Theremin.ino
@@ -9,20 +9,22 @@
 #include "timers.h"
 #include "filter.h"
 
-#define MAX_OUTPUT_FREQUENCY 1500.0
+#define MAX_OUTPUT_FREQUENCY 1760.0
 #define MAX_ULTRASONIC_DISTANCE 60.0  // in cm (trim to this)
 #define MAX_RELIABLE_DISTANCE 120.0  // stop extreme readings from ultrasonics
 #define MAX_WAIT_TIME ((MAX_RELIABLE_DISTANCE * 2) / (100 * 340) * 1.2)
 
 #define FET_PINCHOFF_VOLTAGE 3.3
 #define ARDUINO_MAX_VOLTAGE_OUT 5
+#define continuous 0
+#define discrete 1
 
 // I/O pins
 #define VOLUME_PIN 6
 #define CALIBRATION_BUTTON_PIN 8
 #define MODE_BUTTON_PIN 9
 #define POWER_SWITCH_PIN 10 
-#define POWER_LED_PIN 1  // TODO: Get a pin for this.
+#define POWER_LED_PIN 3  // TODO: Get a pin for this.
 
 // Ultrasonic pins
 #define TRIGGER_PIN 12
@@ -34,7 +36,11 @@ static bool is_ultrasonic_connected = true;
 
 static double abs_min_freq = 30550;
 static double abs_max_freq = 31400;
+static double abs_min_vol = 0.0;
+static double abs_max_vol = 0.0;
+static double previous_vol = 0.0;
 static bool is_power_on = true;
+static bool current_mode;
 
 // float minFreq = 30600.00;
 // float maxFreq = 31360.00;
@@ -80,7 +86,7 @@ void setup()
     pinMode(POWER_SWITCH_PIN, INPUT_PULLUP);
     pinMode(POWER_LED_PIN, OUTPUT);
 
-    digitalWrite(POWER_LED_PIN, HIGH);
+    
     
     // TODO: check inital power switch value.
     
@@ -102,10 +108,17 @@ char distance_to_volume(double distance) {
 }
 
 
-char frequency_to_volume(double frequency) {
-    double vol = (frequency - abs_min_freq) / (abs_max_freq - abs_min_freq) * 255.0;
+
+
+char frequency_to_volume(double frequency) { 
+    double vol = ((frequency - abs_min_freq) / (abs_max_freq - abs_min_freq)) * ((frequency - abs_min_freq) / (abs_max_freq - abs_min_freq)) * 255.0;
+    if (frequency > abs_max_freq || frequency < abs_min_freq) {
+      vol = 0;
+    }
+    Serial.println(vol);
     vol = min(255, vol);
     vol = max(0, vol);
+    //Serial.println(vol);
     return (char) vol;
 }
 
@@ -116,7 +129,7 @@ double calibrate() {
 
     double low_freq = frequency_read(100);  // Wait 100 ms
     //digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("LOW");
+    //Serial.println("LOW");
 
     while (digitalRead(CALIBRATION_BUTTON_PIN)) continue;  // Wait for release.
     delay(100);
@@ -125,13 +138,52 @@ double calibrate() {
     // Insert calibration (high) function.
     double high_freq = frequency_read(100);  // Wait 100 ms
     //digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("HIGH");
+    //Serial.println("HIGH");
 
-    // With 10% padding.
-    abs_min_freq = (high_freq - low_freq) * 0.1 + low_freq;
-    abs_max_freq = high_freq - (high_freq - low_freq) * 0.1;
+    // With 5% padding.
+    abs_min_freq = (high_freq - low_freq) * 0.8 + low_freq;
+    abs_max_freq = high_freq;
 }
 
+double discretization(double frequency_out) {
+  
+   //double frequency_table[37] = {220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30,
+   //                              440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61,
+   //                              880.00, 932.33, 987.77, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22,
+   //                              1760.00};
+
+   double frequency_table[25] = {220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30,
+                                 440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61,
+                                 880.00};
+   //int n = 37;
+   int n = 25;
+   int first, middle, last;
+
+   first = 0;
+   last = n - 1;
+   middle = (first + last) / 2;
+
+   frequency_out = (frequency_out / 1760.00) * (frequency_table[last] - frequency_table[first]) + frequency_table[first];
+ 
+   while (first <= last) {
+      if (frequency_table[middle] < frequency_out) {
+         first = middle + 1;    
+      } else {
+         last = middle - 1;
+      }
+      middle = (first + last)/2;
+   }
+   return frequency_table[middle];   
+
+}
+
+void mode_toggle (void) {
+  if (current_mode == continuous) {
+      current_mode = discrete;
+  } else {
+      current_mode = continuous;
+  }
+}
 
 void buttons() {
 
@@ -140,13 +192,16 @@ void buttons() {
     // Make it go quiet.
     if (!is_power_on) {
         frequency_set(0);
+        digitalWrite(POWER_LED_PIN, LOW);
     }
 
     if (is_power_on) {
-        // if (!digitalRead(MODE_BUTTON_PIN)) {
-        //     //mode_toggle();
-        //     while(!digitalRead(MODE_BUTTON_PIN)) continue;
-        // }
+        digitalWrite(POWER_LED_PIN, HIGH);
+        
+        if (digitalRead(MODE_BUTTON_PIN)) {
+            mode_toggle();
+            while(!digitalRead(MODE_BUTTON_PIN)) continue;
+        }
 
         // Volume control: Calibrate input frequency boundaries from capacitive sensor.
         if (digitalRead(CALIBRATION_BUTTON_PIN)) {
@@ -168,14 +223,17 @@ void loop() {
         double frequency_out = distance_to_frequency(distance_in);
         double frequency_in = frequency_read(16);  // Sample at ~62 Hz (1000/16) from port 5.
 
+        // TODO: Add discrete frequency here.
+        if (current_mode == discrete) {
+            frequency_out = discretization(frequency_out);
+        }
+
         // Clip so that no sound occurs when out of range.
         if (distance_raw > MAX_ULTRASONIC_DISTANCE) {
             frequency_out = 0;
         }
-
-        // TODO: Add discrete frequency here.
-
-        frequency_set(frequency_out);  // Generate on port 11.
+        
+        frequency_set(frequency_out);  // Generate on port 11. 
         char volume = frequency_to_volume(frequency_in);
 
         // (0 - 255 gives 0V - 5V, or low to high volume)
@@ -201,10 +259,4 @@ void loop() {
 //   return duty_cycle;
 // }
 
-// void mode_toggle (void) {
-//   if (current_mode == continuous) {
-// 	current_mode = discrete;
-//   } else {
-// 	current_mode = continuous;
-//   }
-// }
+
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Theremin Design Project
 
 Producing a sine wave using frequency varying PWM output.
+# Theremin-with-discrete-pitch-mode
